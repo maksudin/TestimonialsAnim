bc71a232e927ceeededc7df36fedd921810586de
maksudin <m.iljushkin2@gmail.com>
2019-11-08 15:57:54 +0300

    minor fixes

--- src/js/app.js
+++ src/js/app.js
@@ -15,8 +15,8 @@ TweenMax.to(".person-info__img--large", 0.5, { y: -50, opacity: 1, delay: 0.8});
 // скрытие карточек
 TweenMax.to(cards[6], 0.5, { y: 0, opacity: 0, delay: 4.1 });
 TweenMax.to(cards[5], 0.5, { y: 0, opacity: 0, delay: 4.2 });
-TweenMax.to(".person-info__img--large", 0.5, { y: 0, opacity: 0, delay: 4.2 });
 TweenMax.to(cards[4], 0.5, { y: 0, opacity: 0, delay: 4.3 });
+TweenMax.to(".person-info__img--large", 0.5, { y: 0, opacity: 0, delay: 4.4 });
 TweenMax.to(cards[3], 0.5, { y: 0, opacity: 0, delay: 4.4 });
 TweenMax.to(cards[2], 0.5, { y: 0, opacity: 0, delay: 4.5 });
 TweenMax.to(cards[1], 0.5, { y: 0, opacity: 0, delay: 4.6 });
